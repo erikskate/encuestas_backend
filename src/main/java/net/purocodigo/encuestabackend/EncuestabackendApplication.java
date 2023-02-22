cac4b83545056c548f993ac30ca77d3ad465115f
Erik Joel <erikjoel35@hotmail.com>
2023-02-22 13:58:56 -0600

    Se agrega el entity UserEntity

--- src/main/java/net/purocodigo/encuestabackend/EncuestabackendApplication.java
+++ src/main/java/net/purocodigo/encuestabackend/EncuestabackendApplication.java
@@ -2,9 +2,6 @@ package net.purocodigo.encuestabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class EncuestabackendApplication {
